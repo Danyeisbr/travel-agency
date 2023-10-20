833221fa0565e38f56b1c085b43aee01ad0795ab
Danyeis Bravo Ramirez <danyeistj@gmail.com>
2023-10-20 11:14:34 -0500

    database config updated

--- src/services/databaseService.js
+++ src/services/databaseService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:4000/api/",
+  baseURL: "https://travel-app.azurewebsites.net/api/",
   headers: {
     "Content-Type": "application/json",
   },
