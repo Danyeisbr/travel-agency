5901812d5334f7d90e25c86ceffc0f0f624e1938
Danyeis Bravo Ramirez <danyeistj@gmail.com>
2023-10-20 20:31:44 -0500

    http

--- src/services/databaseService.js
+++ src/services/databaseService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "https://test-17.azurewebsites.net/api/",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "http://test-17.azurewebsites.net/api/",
   headers: {
     "Content-Type": "application/json",
   },
